8248356d57a7a6917f29967d3e8014e2f7689676
KyuriKim <kuyrik1@gmail.com>
2025-03-28 21:06:15 +0900

    25.03.28 - 카카오맵 출발지/도착지 마커 표시

--- lib/location.dart
+++ lib/location.dart
@@ -52,7 +52,9 @@ class _LocationScreenState extends State<LocationScreen> {
 
       // 현재 위치 가져오기
       Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
+          locationSettings: const LocationSettings(
+            accuracy: LocationAccuracy.high,
+          ),
       );
 
       setState(() {
